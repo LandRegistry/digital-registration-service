4d47ba28b5f1b137ba9d40b7a0ff8d5a81414c5f
Ros <YOUR EMAIL ADDRESS>
2019-11-29 23:20:06 +0000

    New Journey

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,25 @@ const path = require('path')
 // Add your routes here - above the module.exports line
 router.use('/node_modules', express.static('node_modules'))
 
+// post(route, callback(request, response) {
+//     response.render(template, data);
+// });
+
+// 
+// // Branching
+// router.post('/examples/branching/over-18-answer', function (req, res) {
+//   // Get the answer from session data
+//   // The name between the quotes is the same as the 'name' attribute on the input elements
+//   // However in JavaScript we can't use hyphens in variable names
+//
+//   let over18 = req.session.data['over-18']
+//
+//   if (over18 === 'false') {
+//     res.redirect('/docs/examples/branching/under-18')
+//   } else {
+//     res.redirect('/docs/examples/branching/over-18')
+//   }
+// })
+
+
 module.exports = router
--- docs/documentation_routes.js
+++ docs/documentation_routes.js
@@ -52,20 +52,20 @@ router.get('/examples/template-data', function (req, res) {
   res.render('examples/template-data', { 'name': 'Foo' })
 })
 
-// Branching
-router.post('/examples/branching/over-18-answer', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
-
-  let over18 = req.session.data['over-18']
-
-  if (over18 === 'false') {
-    res.redirect('/docs/examples/branching/under-18')
-  } else {
-    res.redirect('/docs/examples/branching/over-18')
-  }
-})
+// // Branching
+// router.post('/examples/branching/over-18-answer', function (req, res) {
+//   // Get the answer from session data
+//   // The name between the quotes is the same as the 'name' attribute on the input elements
+//   // However in JavaScript we can't use hyphens in variable names
+//
+//   let over18 = req.session.data['over-18']
+//
+//   if (over18 === 'false') {
+//     res.redirect('/docs/examples/branching/under-18')
+//   } else {
+//     res.redirect('/docs/examples/branching/over-18')
+// //   }
+// })
 
 module.exports = router
 
