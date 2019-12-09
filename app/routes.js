889c76b2d57d9799df0f569d85a1c50be8cac528
Ros <YOUR EMAIL ADDRESS>
2019-12-09 10:26:54 +0000

    Continued

--- app/routes.js
+++ app/routes.js
@@ -6,18 +6,18 @@ const path = require('path')
 router.use('/node_modules', express.static('node_modules'))
 
 
-router.post('/app/views/transferee-select-answer', function (req, res) {
-
-  let transferSelect = req.session.data['transferee-select']
-
-  console.log(transferSelect)
-
-  if (transferSelect == 'false') {
-    res.redirect('/app/views/transactions/transfer/add-transferee')
-  } else {
-    res.redirect('/app/views/transactions/transfer/transferee-list')
-  }
-})
+// router.post('/app/views/transferee-select-answer', function (req, res) {
+//
+//   let transferSelect = req.session.data['transferee-select']
+//
+//   console.log(transferSelect)
+//
+//   if (transferSelect == 'false') {
+//     res.redirect('/app/views/transactions/transfer/add-transferee')
+//   } else {
+//     res.redirect('/app/views/transactions/transfer/transferee-list')
+// //   }
+// })
 
 // post(route, callback(request, response) {
 //     response.render(template, data);
