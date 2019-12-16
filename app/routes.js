4e4e2a6d8f39faec6d25b0b51b475c7526d01f42
Ros <YOUR EMAIL ADDRESS>
2019-12-16 11:10:34 +0000

    Save & resume 0.2

--- app/routes.js
+++ app/routes.js
@@ -88,6 +88,56 @@ router.post('/transactions/transfer/transferee-addresstype-answer', function (re
 
 
 
+// Passing data into a page
+router.get('/stored-data', function (req, res) {
+	console.log(req.session.data)
+  res.render('stored-data')
+})
+
+
+router.get('/docs/examples/pass-data/vehicle-registration-clean', function (req, res) {
+	req.session.data = {}
+  res.redirect('vehicle-registration')
+})
+
+
+router.get('/docs/examples/pass-data/vehicle-registration-car1', function (req, res) {
+	req.session.data = {
+  "vehicle-registration": "test-plate",
+  "vehicle-type": "Car",
+  "vehicle-features": [
+    "Heated seats",
+    "Radio"
+  ]
+}
+  res.redirect('vehicle-registration')
+})
+
+router.get('/docs/examples/pass-data/vehicle-registration-car2', function (req, res) {
+	req.session.data = {
+  "vehicle-registration": "BO12 3XX",
+  "vehicle-type": "Car",
+  "vehicle-features": [
+    "Heated seats",
+    "GPS",
+    "Radio"
+  ]
+}
+  res.redirect('vehicle-registration')
+})
+
+router.get('/docs/examples/pass-data/vehicle-registration-lorry1', function (req, res) {
+	req.session.data = {
+  "vehicle-registration": "LR56 RRY",
+  "vehicle-type": "Lorry",
+  "vehicle-features": [
+    "GPS",
+    "Radio"
+  ]
+}
+  res.redirect('vehicle-registration')
+})
+
 
 
 
